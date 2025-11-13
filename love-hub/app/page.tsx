58b5df4b68a8d235846ee7c29337bd7b24af1394
yPabloo <pablo.afreire@gmail.com>
2025-11-13 18:37:36 -0300

    :heavy_plus_sign:build:Instalacao do Next React e suas dependencias

---
+++ love-hub/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-pink-300">
+      <h1 className="text-4xl font-bold text-pink-600">
+        Te amo, meu amor ❤️
+      </h1>
+    </main>
+  );
+}
+
